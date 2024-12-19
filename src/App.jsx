009b82da7b097984d0c4dc8065a355dda9f5af31
Ugo <ugoezeokoli@gmail.com>
2024-12-18 22:36:16 -0800

    added end time feature, made timer count down from time required for item, added current time display

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ function App() {
   const [item, setItem] = useState("")
   const [hours, setHours] = useState(0)
   const [minutes, setMinutes] = useState(0)
+  const [endTimeInput, setEndTimeInput] = useState("")
+  const [endTime, setEndTime] = useState("")
   
   const addItem = () => {
-    console.log(item)
-    setItems(items.concat(item))
+    console.log("item: ",item)
+    const itemObj = {name: item, hours: hours, minutes:minutes}
+    setItems(items.concat(itemObj))
     setItem("")
   }
 
@@ -17,9 +20,32 @@ function App() {
       addItem()
     }
   }
-  const changeInput = (event) => {
+  const changeItemInput = (event) => {
     const new_item = event.target.value
     setItem(new_item)
+  }
+  const changeEndInput = (event) => {
+    const new_time = event.target.value
+    // console.log("change end input: ", new_time)
+    setEndTimeInput(new_time)
+  }
+  
+  const confirmEndTime = () => {
+    const timeString = endTimeInput
+    const now = new Date()
+    const now_string = now.toISOString().split('T')[0] // YYYY-MM-DD
+
+    const endTimeString = `${now_string} ${timeString}`
+    const endTimeObject = new Date(endTimeString)
+    
+    if (endTimeObject == "Invalid Date" ){
+      // console.log("works!")
+      window.alert("Invalid Input")
+    }
+    else{
+      setEndTime(endTimeObject)
+      setEndTimeInput("")
+    }
   }
 
   const addHour = () => {
@@ -50,14 +76,46 @@ function App() {
   }
   return (
     <div>
+      <CurrentTime/>
+      <p>End Time: {endTime ? endTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: true }): "End time not entered!"}</p>
       <Table items={items}/>
       {/* <InputTime text={"time required: "}/> */}
-      <div>item: <input value={item} onChange={changeInput} onKeyDown={keyDownAddItem}/></div>
-      {/* <div>time required <input value={item} onChange={changeInput} onKeyDown={keyDownAddItem}/></div> */}
+      <div>item: <input value={item} onChange={changeItemInput} onKeyDown={keyDownAddItem}/></div>
+      {/* <div>time required <input value={item} onChange={changeItemInput} onKeyDown={keyDownAddItem}/></div> */}
       <InputTime text={"time required: "} hours={hours} minutes ={minutes} addHour={addHour} addMinute={addMinute} subHour={subHour} subMinute={subMinute}/>
       {/* <Button onClick={addItem}/> */}
       <Button onClick={addItem}text={"add item"}/>
+      <EndInput EndTime ={endTimeInput} onChange={changeEndInput}/>
+      <Button onClick={confirmEndTime} text={"confirm end time"}/>
     </div>
+  )
+}
+
+const EndInput = (props) => {
+
+  return(
+    <div>
+      Enter end time: <input value={props.EndTime} onChange={props.onChange} placeholder='ex: 10:30 PM'/>
+    </div>
+  )
+}
+const CurrentTime = () => {
+  const now = new Date()
+  const minutes_now = now.getMinutes()
+  const hours_now = now.getHours()
+  const [hours, setHours] = useState(hours_now)
+  const [minutes, setMinutes] = useState(minutes_now)
+
+  const formatted_time = `${hours > 12 ? hours - 12 : hours}:${minutes.toString().length == 1? `0${minutes}`:minutes} ${hours > 12 ? "PM" : "AM"}`
+  setTimeout(()=> {
+    const new_now = new Date()
+    const minutes_now = new_now.getMinutes()
+    const hours_now = new_now.getHours()
+    setHours(hours_now)
+    setMinutes(minutes_now)
+  },60000)
+  return (
+      <p>Current Time: {formatted_time}</p>
   )
 }
 
@@ -67,8 +125,13 @@ const InputTime = (props) => {
   return(
     <div> {props.text}
     <Button text={"+"} onClick={props.addHour}/>
-    <input value ={hours}key={"hours"}/>
-    <Button text={"-"} onClick = {props.subHour}/>: <Button text={"+"} onClick={props.addMinute}/><input value = {minutes}key={"minutes"}/><Button text={"-"} onClick={props.subMinute}/>
+    {/* <input value ={hours}key={"hours"}/> */}
+    <b>{hours}</b>
+    <Button text={"-"} onClick = {props.subHour}/>: 
+    <Button text={"+"} onClick={props.addMinute}/>
+    {/* <input value = {minutes.toString().length == 1? `0${minutes}`: minutes}key={"minutes"}/> */}
+    <b>{minutes.toString().length == 1? `0${minutes}`: minutes}</b>
+    <Button text={"-"} onClick={props.subMinute}/>
     </div>
   )
 }
@@ -79,20 +142,19 @@ const Button  = (props) => {
 }
 
 const Table = (props) => {
-  console.log("in table component",props.items)
+  // console.log("in table component",props.items)
   return(
     <table>
       <thead>
         <tr>
           <th>item</th>
           <th>timer</th>
-          <th>ends</th>
         </tr>
         {props.items.map(item => {
           return(
-            <tr key={item}>
-              <td >{item}</td>
-              <td><Timer/></td>
+            <tr key={item.name}>
+              <td >{item.name}</td>
+              <td><Timer hours={item.hours} minutes={item.minutes}/></td>
             </tr>
           )
         })}
@@ -102,14 +164,17 @@ const Table = (props) => {
 }
 const Timer = (props) => {
   const [seconds, setSeconds] = useState(0)
-  const [minutes, setMinutes] = useState(0)
+  const [minutes, setMinutes] = useState(((props.hours * 60) + (props.minutes)))
 
-  if (seconds == 59){
-    setTimeout(() => setSeconds(0), 1000)
-    setTimeout(() => setMinutes(minutes + 1), 1000)
+  if (seconds == 0 && minutes == 0){
+    return <p>Finished!</p>
+  }
+  else if (seconds == 0){
+    setTimeout(() => setSeconds(59), 1000)
+    setTimeout(() => setMinutes(minutes - 1), 1000)
   }
   else {
-    setTimeout(() => setSeconds(seconds + 1), 1000)
+    setTimeout(() => setSeconds(seconds - 1), 1000)
   }
 
   return(
